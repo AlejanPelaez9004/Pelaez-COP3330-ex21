b115417572930d200bf832938f9094842c6eb38e
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-09-12 15:53:39 -0700

    Complete

--- src/main/java/Pelaez/Alejandro/App.java
+++ src/main/java/Pelaez/Alejandro/App.java
@@ -8,10 +8,64 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args)
     {
-        System.out.print("What is your name?");
+        System.out.print("Please enter the number of the month: ");
         Scanner input = new Scanner(System.in);
-        String name = input.next();
-        System.out.print("Hello " + name + ", nice to meet you!");
-        System.out.print("\n");
+        int numMonth = input.nextInt();
+        String month = "";
+        switch (numMonth)
+        {
+            case 1: {
+                month = "January";
+                break;
+            }
+            case 2: {
+                month = "February";
+                break;
+            }
+            case 3: {
+                month = "March";
+                break;
+            }
+            case 4: {
+                month = "April";
+                break;
+            }
+            case 5: {
+                month = "May";
+                break;
+            }
+            case 6: {
+                month = "June";
+                break;
+            }
+            case 7: {
+                month = "July";
+                break;
+            }
+            case 8: {
+                month = "August";
+                break;
+            }
+            case 9: {
+                month = "September";
+                break;
+            }
+            case 10: {
+                month = "October";
+                break;
+            }
+            case 11: {
+                month = "November";
+                break;
+            }
+            case 12: {
+                month = "December";
+                break;
+            }
+            default:
+                break;
+        }
+
+        System.out.printf("The name of the month is %s.\n", month);
     }
 }
